a259dda4708c41f48509f6ddff2db71d0922c8fe
Yovko Garkov <iovko1993@gmail.com>
2021-04-28 19:27:57 +0300

    Add Retake Exam 21 April 2019

---
+++ CSharp_OOP/OOPAdvancedExamRetake-21April2019/01. Business Logic_Skeleton (.NET Core)/HAD/Entities/Items/RecipeItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HAD.Entities.Items
+{
+    public class RecipeItem : BaseItem
+    {
+        private List<CommonItem> requiredItems;
+
+        public RecipeItem(string name, long strengthBonus, long agilityBonus, long intelligenceBonus, long hitPointsBonus, long damageBonus, params string[] items) 
+             : base(name, strengthBonus, agilityBonus, intelligenceBonus, hitPointsBonus, damageBonus)
+        {
+
+        }
+    }
+}
