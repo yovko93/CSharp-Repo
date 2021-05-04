b4ea3d7265f784d5becb30af8059cc243e299a7e
Yovko Garkov <iovko1993@gmail.com>
2021-05-04 20:34:52 +0300

    Add XML Processing

---
+++ Entity_Framework_Core/09.XML-Processing/Product-Shop/ProductShop/Dtos/Export/ExportUsersWithCountAndProductsDto.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace ProductShop.Dtos.Export
+{
+    [XmlType("Users")]
+    public class ExportUsersWithCountAndProductsDto
+    {
+        [XmlElement("count")]
+        public int Count { get; set; }
+
+        [XmlArray("users")]
+        public List<ExportUserWithProductsDto> Users { get; set; }
+    }
+}
