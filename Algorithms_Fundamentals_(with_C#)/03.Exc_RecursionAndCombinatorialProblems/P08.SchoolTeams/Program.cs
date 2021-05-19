5fbac1d77a8b74c20fd93b7213a48a531502686c
Yovko Garkov <iovko1993@gmail.com>
2021-05-19 12:57:37 +0300

    Update Program.cs

--- Algorithms_Fundamentals_(with_C#)/03.Exc_RecursionAndCombinatorialProblems/P08.SchoolTeams/Program.cs
+++ Algorithms_Fundamentals_(with_C#)/03.Exc_RecursionAndCombinatorialProblems/P08.SchoolTeams/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-using System.IO;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace P08.SchoolTeams
 {
@@ -11,6 +12,42 @@ namespace P08.SchoolTeams
             var boys = Console.ReadLine().Split(", ");
 
 
+            var girlsComb = new string[3];
+            var girlsList = new List<string[]>();
+            Comb(0, 0, girlsComb, girls, girlsList);
+
+            var boysComb = new string[2];
+            var boysList = new List<string[]>();
+            Comb(0, 0, boysComb, boys, boysList);
+
+            foreach (var currentGirlsComb in girlsList)
+            {
+                foreach (var currentBoysComb in boysList)
+                {
+                    Console.WriteLine($"{string.Join(", ", currentGirlsComb)}, {string.Join(", ", currentBoysComb)}");
+                }
+            }
+
+        }
+
+        private static void Comb(
+            int cellIdx, 
+            int elementIdx,
+            string[] comb,
+            string[] elements,
+            List<string[]> result)
+        {
+            if (cellIdx >= comb.Length)
+            {
+                result.Add(comb.ToArray());
+                return;
+            }
+
+            for (int i = elementIdx; i < elements.Length; i++)
+            {
+                comb[cellIdx] = elements[i];
+                Comb(cellIdx + 1, i + 1, comb, elements, result);
+            }
         }
     }
 }
