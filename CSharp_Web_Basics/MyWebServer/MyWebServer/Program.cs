989cdd82287ad9cf62f6f35a38cae26bb548014a
Yovko Garkov <iovko1993@gmail.com>
2021-06-01 19:53:27 +0300

    Update Program.cs

--- CSharp_Web_Basics/MyWebServer/MyWebServer/Program.cs
+++ CSharp_Web_Basics/MyWebServer/MyWebServer/Program.cs
@@ -1,27 +1,18 @@
-﻿using System;
-using System.Net;
-using System.Net.Sockets;
+﻿using MyWebServer.Server;
 using System.Threading.Tasks;
 
 namespace MyWebServer
 {
-    class Program
+    public class Program
     {
         public static async Task Main()
         {
             // http://localhost:9090
 
-            var address = IPAddress.Parse("127.0.0.1");
-            var port = 9090;
+            var server = new HttpServer("127.0.0.1", 9090);
 
-            var serverListener = new TcpListener(address, port);
-
-            serverListener.Start();
-
-            Console.WriteLine($"Server started on port {port}...");
-            Console.WriteLine("Listening for request...");
-            var connection = await serverListener.AcceptTcpClientAsync();
-
+            await server.Start();
+            
         }
     }
 }
