84f1a9409d40d39eebe446fcf0112b4424cbdb18
Yovko Garkov <iovko1993@gmail.com>
2021-06-03 15:23:21 +0300

    Add My Web Server

---
+++ CSharp_Web_Basics/MyWebServer/MyWebServer.Server/Http/HttpHeaderCollection.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace MyWebServer.Server.Http
+{
+    public class HttpHeaderCollection
+    {
+        private readonly Dictionary<string, HttpHeader> headers;
+
+        public HttpHeaderCollection()
+            => this.headers = new Dictionary<string, HttpHeader>();
+
+        public int Count
+           => this.headers.Count;
+
+        public void Add(HttpHeader header)
+            => this.headers.Add(header.Name, header);
+
+
+    }
+}
---
+++ CSharp_Web_Basics/MyWebServer/MyWebServer.Server/HttpServer.cs
@@ -0,0 +1,105 @@
+﻿
+namespace MyWebServer.Server
+{
+    using MyWebServer.Server.Http;
+    using System;
+    using System.Net;
+    using System.Net.Sockets;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class HttpServer
+    {
+        private readonly IPAddress ipAddress;
+        private readonly int port;
+        private readonly TcpListener tcpListener;
+
+        public HttpServer(string ipAddress, int port)
+        {
+            this.ipAddress = IPAddress.Parse(ipAddress);
+            this.port = port;
+
+            tcpListener = new TcpListener(this.ipAddress, port);
+        }
+
+        public async Task Start()
+        {
+            this.tcpListener.Start();
+
+            Console.WriteLine($"Server started on port {port}...");
+            Console.WriteLine("Listening for request...");
+
+            while (true)
+            {
+                var connection = await this.tcpListener.AcceptTcpClientAsync();
+
+                var networkStream = connection.GetStream();
+
+                var requestText = await this.ReadRequest(networkStream);
+
+                Console.WriteLine(requestText);
+
+                var request = HttpRequest.Parse(requestText);
+
+                await WriteResponse(networkStream);
+
+                connection.Close();
+            }
+        }
+
+        private async Task<string> ReadRequest(NetworkStream networkStream)
+        {
+            var bufferLength = 1024;
+            var buffer = new byte[bufferLength];
+
+            var totalBytes = 0;
+
+            var requestBuilder = new StringBuilder();
+
+            do
+            {
+                var bytesRead = await networkStream.ReadAsync(buffer, 0, bufferLength);
+
+                totalBytes += bytesRead;
+
+                if (totalBytes > 10 * 1024)
+                {
+                    throw new InvalidOperationException("Request is too large.");
+                }
+
+                requestBuilder.Append(Encoding.UTF8.GetString(buffer, 0, bytesRead));
+            }
+            while (networkStream.DataAvailable);
+
+            return requestBuilder.ToString();
+        }
+
+        private async Task WriteResponse(NetworkStream networkStream)
+        {
+            var content = @"
+<html>
+    <head>
+        <link rel=""icon"" href=""data:,"">
+    </head>
+    <body>
+        Здрасти от Йовко!
+    </body>
+</html>";
+            var contentLength = Encoding.UTF8.GetByteCount(content);
+
+            var response = $@"
+HTTP/1.1 200 OK
+Server: My Web Server
+Date: {DateTime.UtcNow:r}
+Content-Length: {contentLength}
+Content-Type: text/html; charset=UTF-8
+
+{content}";
+
+            var responseBytes = Encoding.UTF8.GetBytes(response);
+
+            await networkStream.WriteAsync(responseBytes);
+
+        }
+    }
+}
