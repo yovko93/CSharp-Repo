7c8b69be60200da7b265560d03745624904a7759
Yovko Garkov <iovko1993@gmail.com>
2021-06-05 12:36:09 +0300

    Add My Web Server

---
+++ CSharp_Web_Basics/MyWebServer/MyWebServer/StartUp.cs
@@ -0,0 +1,17 @@
+﻿using MyWebServer.Server;
+using MyWebServer.Server.Responses;
+using System.Threading.Tasks;
+
+namespace MyWebServer
+{
+    public class StartUp
+    {
+        public static async Task Main()
+            => await new HttpServer(routes => routes
+                .MapGet("/", new TextResponse("Hello from Yovko!"))
+                .MapGet("/Cats", new TextResponse("<h1>Hello from the cats!</h1>", "text/html"))
+            .MapGet("/Dogs", new TextResponse("<h1>Hello from the dogs!</h1>", "text/html")))
+            .Start();
+
+    }
+}
