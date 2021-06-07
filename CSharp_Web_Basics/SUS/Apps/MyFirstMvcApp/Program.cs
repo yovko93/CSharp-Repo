b72925dfbada1d690851b6977d1fd8a125df4ea4
Yovko Garkov <iovko1993@gmail.com>
2021-06-07 11:36:11 +0300

    Add SUS

---
+++ CSharp_Web_Basics/SUS/Apps/MyFirstMvcApp/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using SUS.HTTP;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace MyFirstMvcApp
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //Console.OutputEncoding = Encoding.UTF8;
+
+            IHttpServer server = new HttpServer();
+
+            server.AddRoute("/", HomePage);
+            server.AddRoute("/favicon.ico", Favicon);
+            server.AddRoute("/about", About); 
+            server.AddRoute("/users/login", Login);
+
+            await server.StartAsync(80);
+
+        }
+
+        static HttpResponse HomePage(HttpRequest request)
+        {
+            var responseHtml = "<h1>Welcome!</h1>" + request.Headers.FirstOrDefault(x => x.Name == "User-Agent")?.Value;
+            var responseBodyBytes = Encoding.UTF8.GetBytes(responseHtml);
+            var response = new HttpResponse("text/html", responseBodyBytes);
+
+            return response;
+        }
+
+        static HttpResponse Favicon(HttpRequest request)
+        {
+            var fileBytes = File.ReadAllBytes("wwwroot/favicon.ico");
+            var response = new HttpResponse("image/vnd.microsoft.ico", fileBytes);
+
+            return response;
+        }
+
+        static HttpResponse About(HttpRequest request)
+        {
+            var responseHtml = "<h1>About...</h1>";
+            var responseBodyBytes = Encoding.UTF8.GetBytes(responseHtml);
+            var response = new HttpResponse("text/html", responseBodyBytes);
+           
+            return response;
+        }
+
+        static HttpResponse Login(HttpRequest request)
+        {
+            var responseHtml = "<h1>Login</h1>";
+            var responseBodyBytes = Encoding.UTF8.GetBytes(responseHtml);
+            var response = new HttpResponse("text/html", responseBodyBytes);
+
+            return response;
+        }
+    }
+}
