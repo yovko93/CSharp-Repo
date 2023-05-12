f03f89f34fdd67144d5213ff94ece902852a8716
Yovko Garkov <iovko1993@gmail.com>
2023-05-12 17:10:51 +0300

    Add queue implementation

--- Data_Structures_Fundamentals_(with C#)/02. Linear-Data-Structures/Problem03.Queue/Queue.cs
+++ Data_Structures_Fundamentals_(with C#)/02. Linear-Data-Structures/Problem03.Queue/Queue.cs
@@ -8,41 +8,104 @@
     {
         private class Node
         {
-            
+            public T Element { get; set; }
+            public Node Next { get; set; }
+
+            public Node(T element, Node next)
+            {
+                this.Element = element;
+                this.Next = next;
+            }
+
+            public Node(T element)
+            {
+                this.Element = element;
+            }
         }
 
         private Node head;
 
-        public int Count => throw new NotImplementedException();
+        public int Count { get; private set; }
 
         public void Enqueue(T item)
         {
-            throw new NotImplementedException();
+            var newNode = new Node(item);
+
+            if (this.head == null)
+            {
+                this.head = newNode;
+            }
+            else
+            {
+                var node = this.head;
+                while (node.Next != null)
+                {
+                    node = node.Next;
+                }
+
+                node.Next = newNode;
+            }
+
+            this.Count++;
         }
 
         public T Dequeue()
         {
-            throw new NotImplementedException();
+            this.EnsureNotEmpty();
+
+            var oldHead = this.head;
+            this.head = oldHead.Next;
+            this.Count--;
+
+            return oldHead.Element;
         }
 
         public T Peek()
         {
-            throw new NotImplementedException();
+            this.EnsureNotEmpty();
+
+            var node = this.head;
+            return node.Element;
         }
 
         public bool Contains(T item)
         {
-            throw new NotImplementedException();
+            var node = this.head;
+            while (node != null)
+            {
+                if (node.Element.Equals(item))
+                {
+                    return true;
+                }
+
+                node = node.Next;
+            }
+
+            return false;
         }
 
         public IEnumerator<T> GetEnumerator()
         {
-            throw new NotImplementedException();
+            var node = this.head;
+
+            while (node != null)
+            {
+                yield return node.Element;
+                node = node.Next;
+            }
         }
 
         IEnumerator IEnumerable.GetEnumerator()
+            => this.GetEnumerator();
+
+        #region Helpers
+        private void EnsureNotEmpty()
         {
-            throw new NotImplementedException();
+            if (this.head == null)
+            {
+                throw new InvalidOperationException();
+            }
         }
+        #endregion
     }
 }
