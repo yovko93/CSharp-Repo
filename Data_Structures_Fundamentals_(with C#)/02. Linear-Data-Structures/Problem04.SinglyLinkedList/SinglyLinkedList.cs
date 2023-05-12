0ed81ffec3d6b194625534beea2ceeb0e5523836
Yovko Garkov <iovko1993@gmail.com>
2023-05-12 18:33:12 +0300

    Add singly linked list

--- Data_Structures_Fundamentals_(with C#)/02. Linear-Data-Structures/Problem04.SinglyLinkedList/SinglyLinkedList.cs
+++ Data_Structures_Fundamentals_(with C#)/02. Linear-Data-Structures/Problem04.SinglyLinkedList/SinglyLinkedList.cs
@@ -6,46 +6,134 @@
 
     public class SinglyLinkedList<T> : IAbstractLinkedList<T>
     {
-        public int Count => throw new NotImplementedException();
+        private class Node
+        {
+            public T Element { get; set; }
+            public Node Next { get; set; }
+
+            public Node(T element, Node next)
+            {
+                this.Element = element;
+                this.Next = next;
+            }
+
+            public Node(T element)
+            {
+                this.Element = element;
+            }
+        }
+
+        private Node head;
+
+        public int Count { get; private set; }
 
         public void AddFirst(T item)
         {
-            throw new NotImplementedException();
+            var node = new Node(item, this.head);
+            this.head = node;
+            this.Count++;
         }
 
         public void AddLast(T item)
         {
-            throw new NotImplementedException();
-        }
+            var newNode = new Node(item);
 
-        public IEnumerator<T> GetEnumerator()
-        {
-            throw new NotImplementedException();
+            if (this.head == null)
+            {
+                this.head = newNode;
+            }
+            else
+            {
+                var node = this.head;
+
+                while (node.Next != null)
+                {
+                    node = node.Next;
+                }
+
+                node.Next = newNode;
+            }
+
+            this.Count++;
         }
 
         public T GetFirst()
         {
-            throw new NotImplementedException();
+            this.EnsureNotEmpty();
+
+            var node = this.head;
+            return node.Element;
         }
 
         public T GetLast()
         {
-            throw new NotImplementedException();
+            this.EnsureNotEmpty();
+
+            var node = this.head;
+            while (node.Next != null)
+            {
+                node = node.Next;
+            }
+
+            return node.Element;
         }
 
         public T RemoveFirst()
         {
-            throw new NotImplementedException();
+            this.EnsureNotEmpty();
+
+            var oldHead = this.head;
+            this.head = oldHead.Next;
+            this.Count--;
+
+            return oldHead.Element;
         }
 
         public T RemoveLast()
         {
-            throw new NotImplementedException();
+            this.EnsureNotEmpty();
+            var node = this.head;
+
+            if (this.Count == 1)
+            {
+                this.head = null;
+                this.Count--;
+                return node.Element;
+            }
+
+            while (node.Next.Next != null)
+            {
+                node = node.Next;
+            }
+
+            var deletedElement = node.Next;
+            node.Next = null;
+            this.Count--;
+            return deletedElement.Element;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            var node = this.head;
+
+            while (node != null)
+            {
+                yield return node.Element;
+                node = node.Next;
+            }
         }
 
         IEnumerator IEnumerable.GetEnumerator()
+            => this.GetEnumerator();
+
+        #region Helpers
+        private void EnsureNotEmpty()
         {
-            throw new NotImplementedException();
+            if (this.head == null)
+            {
+                throw new InvalidOperationException();
+            }
         }
+        #endregion
     }
 }
