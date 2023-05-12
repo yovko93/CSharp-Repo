9654e7881a72136f95803bdc7a7914684f2bd198
Yovko Garkov <iovko1993@gmail.com>
2023-05-12 14:34:05 +0300

    Add stack implementation

--- Data_Structures_Fundamentals_(with C#)/02. Linear-Data-Structures/Problem02.Stack/Stack.cs
+++ Data_Structures_Fundamentals_(with C#)/02. Linear-Data-Structures/Problem02.Stack/Stack.cs
@@ -8,41 +8,89 @@
     {
         private class Node
         {
-            
+            public T Element { get; set; }
+            public Node Next { get; set; }
+
+            public Node(T element, Node next)
+            {
+                this.Element = element;
+                this.Next = next;
+            }
+
+            public Node(T element)
+            {
+                this.Element = element;
+            }
         }
 
         private Node top;
 
-        public int Count => throw new System.NotImplementedException();
+        public int Count { get; private set; }
 
         public void Push(T item)
         {
-            throw new NotImplementedException();
+            var node = new Node(item, this.top);
+            this.top = node;
+            this.Count++;
         }
 
         public T Pop()
         {
-            throw new System.NotImplementedException();
+            this.ValidateNode();
+
+            var oldTop = this.top;
+            this.top = oldTop.Next;
+            this.Count--;
+
+            return oldTop.Element;
         }
 
         public T Peek()
         {
-            throw new System.NotImplementedException();
+            this.ValidateNode();
+
+            return this.top.Element;
         }
 
         public bool Contains(T item)
         {
-            throw new System.NotImplementedException();
+            var node = this.top;
+
+            while (node != null)
+            {
+                if (node.Element.Equals(item))
+                {
+                    return true;
+                }
+
+                node = node.Next;
+            }
+
+            return false;
         }
 
         public IEnumerator<T> GetEnumerator()
         {
-            throw new NotImplementedException();
+            var node = this.top;
+
+            while (node != null)
+            {
+                yield return node.Element;
+                node = node.Next;
+            }
         }
 
         IEnumerator IEnumerable.GetEnumerator()
+            => this.GetEnumerator();
+
+        #region Privates
+        private void ValidateNode()
         {
-            throw new NotImplementedException();
+            if (this.top == null)
+            {
+                throw new InvalidOperationException();
+            }
         }
+        #endregion
     }
 }
