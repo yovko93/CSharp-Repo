b58014cffcf488e7e33cd70b467cfd801c9f0040
Yovko Garkov <iovko1993@gmail.com>
2023-09-26 22:17:49 +0300

    Implement DoublyLinkedList

--- Data_Structures_Fundamentals_(with C#)/03. Linear-Data-Structures-Exercise/02.DoublyLinkedList/DoublyLinkedList.cs
+++ Data_Structures_Fundamentals_(with C#)/03. Linear-Data-Structures-Exercise/02.DoublyLinkedList/DoublyLinkedList.cs
@@ -25,40 +25,123 @@
 
         public void AddFirst(T item)
         {
-            throw new NotImplementedException();
+            var newNode = new Node(item);
+
+            if (this.head == null)
+            {
+                this.head = newNode;
+                this.tail = newNode;
+
+                //this.head = this.tail = newNode;
+            }
+            else
+            {
+                this.head.Previous = newNode;
+                newNode.Next = this.head;
+                this.head = newNode;
+            }
+
+            Count++;
         }
 
         public void AddLast(T item)
         {
-            throw new NotImplementedException();
+            var newNode = new Node(item);
+
+            if (this.head == null)
+            {
+                this.head = newNode;
+                this.tail = newNode;
+            }
+            else
+            {
+                newNode.Previous = this.tail;
+                this.tail.Next = newNode;
+                this.tail = newNode;
+            }
+
+            this.Count++;
         }
 
         public T GetFirst()
         {
-            throw new NotImplementedException();
+            this.EnsureNotEmpty();
+
+            return this.head.Value;
         }
 
         public T GetLast()
         {
-            throw new NotImplementedException();
+            this.EnsureNotEmpty();
+
+            return this.tail.Value;
         }
 
         public T RemoveFirst()
         {
-            throw new NotImplementedException();
+            this.EnsureNotEmpty();
+
+            var deletedItem = this.head;
+
+            if (this.Count == 1) // this.head.Next == null
+            {
+                this.head = null;
+                this.tail = null;
+            }
+            else
+            {
+                this.head = this.head.Next;
+                this.head.Previous = null;
+            }
+
+            this.Count--;
+
+            return deletedItem.Value;
         }
 
         public T RemoveLast()
         {
-            throw new NotImplementedException();
+            this.EnsureNotEmpty();
+
+            var deletedItem = this.tail;
+
+            if (this.tail.Previous == null) //this.Count == 1)
+            {
+                this.head = null;
+                this.tail = null;
+            }
+            else
+            {
+                this.tail = this.tail.Previous;
+                this.tail.Next = null;
+            }
+
+            this.Count--;
+            return deletedItem.Value;
         }
 
         public IEnumerator<T> GetEnumerator()
         {
-            throw new NotImplementedException();
+            var node = this.head;
+
+            while (node != null)
+            {
+                yield return node.Value;
+                node = node.Next;
+            }
         }
 
         IEnumerator IEnumerable.GetEnumerator()
             => this.GetEnumerator();
+
+        #region Helpers
+        private void EnsureNotEmpty()
+        {
+            if (this.Count == 0)
+            {
+                throw new InvalidOperationException();
+            }
+        }
+        #endregion
     }
 }
