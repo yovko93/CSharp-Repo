b2d862a0022047e9f2d7210f32ad232fa6ce2776
Yovko Garkov <iovko1993@gmail.com>
2023-12-05 16:23:38 +0200

    Implement AVL tree

--- Data_Structures_Advanced_(with_C#)/04.AVL-Trees-and-AA-Trees-Exercise/AVLTree/AVL.cs
+++ Data_Structures_Advanced_(with_C#)/04.AVL-Trees-and-AA-Trees-Exercise/AVLTree/AVL.cs
@@ -9,39 +9,243 @@
             public Node(T value)
             {
                 this.Value = value;
+                this.Height = 1;
             }
 
             public T Value { get; set; }
             public Node Left { get; set; }
             public Node Right { get; set; }
             public int Height { get; set; }
+
+            public override string ToString()
+            {
+                return $"V: [{Value}] R: [{Right}] L: [{Left}] H: [{Height}]";
+            }
         }
 
         public Node Root { get; private set; }
 
         public bool Contains(T element)
         {
-            throw new InvalidOperationException();
+            return this.Contains(this.Root, element) != null;
+        }
+
+        private Node Contains(Node node, T element)
+        {
+            if (node == null)
+            {
+                return null;
+            }
+
+            var compare = element.CompareTo(node.Value);
+
+            if (compare < 0)
+            {
+                return this.Contains(node.Left, element);
+            }
+            else if (compare > 0)
+            {
+                return this.Contains(node.Right, element);
+            }
+
+            return node;
         }
 
         public void Delete(T element)
         {
-            throw new InvalidOperationException();
+            this.Root = this.Delete(this.Root, element);
+        }
+
+        private Node Delete(Node node, T element)
+        {
+            if (node == null)
+            {
+                return null;
+            }
+
+            if (element.CompareTo(node.Value) < 0)
+            {
+                node.Left = this.Delete(node.Left, element);
+            }
+            else if (element.CompareTo(node.Value) > 0)
+            {
+                node.Right = this.Delete(node.Right, element);
+            }
+            else
+            {
+                //// One or zero children
+                //if (node.Left == null || node.Right == null)
+                //{
+                //    Node temp;
+                //    if (node.Left == null)
+                //    {
+                //        temp = node.Right;
+                //    }
+                //    else
+                //    {
+                //        temp = node.Left;
+                //    }
+
+                //    if (temp == null)
+                //    {
+                //        return null;
+                //    }
+                //    else
+                //    {
+                //        node = temp;
+                //    }
+                //}
+                if (node.Left == null && node.Right == null)
+                {
+                    return null;
+                }
+                else if (node.Left == null)
+                {
+                    node = node.Right;
+                }
+                else if (node.Right == null)
+                {
+                    node = node.Left;
+                }
+                else
+                {
+                    Node temp = this.FindSmallestChild(node.Right);
+                    node.Value = temp.Value;
+
+                    node.Right = this.Delete(node.Right, temp.Value);
+                }
+            }
+
+            node = this.Balance(node);
+            node.Height = Math.Max(Height(node.Left), Height(node.Right)) + 1;
+
+            return node;
+        }
+
+        private Node FindSmallestChild(Node node)
+        {
+            if (node.Left == null)
+            {
+                return node;
+            }
+
+            return this.FindSmallestChild(node.Left);
         }
 
         public void DeleteMin()
         {
-            throw new InvalidOperationException();
+            if (this.Root == null)
+            {
+                return;
+            }
+
+            var temp = this.FindSmallestChild(this.Root);
+
+            this.Root = this.Delete(this.Root, temp.Value);
         }
 
         public void Insert(T element)
         {
-            throw new InvalidOperationException();
+            this.Root = this.Insert(this.Root, element);
+        }
+
+        private Node Insert(Node node, T element)
+        {
+            if (node == null)
+            {
+                return new Node(element);
+            }
+
+            if (element.CompareTo(node.Value) < 0)
+            {
+                node.Left = this.Insert(node.Left, element);
+            }
+            else
+            {
+                node.Right = this.Insert(node.Right, element);
+            }
+
+            node = this.Balance(node);
+            node.Height = Math.Max(Height(node.Left), Height(node.Right)) + 1;
+
+            return node;
+        }
+
+        private Node Balance(Node node)
+        {
+            var balanceFactor = Height(node.Left) - Height(node.Right);
+
+            if (balanceFactor > 1)
+            {
+                var childBalanceFactor = Height(node.Left.Left) - Height(node.Left.Right);
+                if (childBalanceFactor < 0)
+                {
+                    node.Left = RotateLeft(node.Left);
+                }
+
+                node = RotateRight(node);
+            }
+            else if (balanceFactor < -1)
+            {
+                var childBalanceFactor = Height(node.Right.Left) - Height(node.Right.Right);
+                if (childBalanceFactor > 0)
+                {
+                    node.Right = RotateRight(node.Right);
+                }
+
+                node = RotateLeft(node);
+            }
+
+            return node;
+        }
+
+        private Node RotateRight(Node node)
+        {
+            var left = node.Left;
+            node.Left = left.Right; // node.Left.Right;
+            left.Right = node;
+
+            node.Height = Math.Max(Height(node.Left), Height(node.Right)) + 1;
+
+            return left;
+        }
+
+        private Node RotateLeft(Node node)
+        {
+            var right = node.Right;
+            node.Right = right.Left; // node.Right.Left
+            right.Left = node;
+
+            node.Height = Math.Max(Height(node.Left), Height(node.Right)) + 1;
+
+            return right;
+        }
+
+        private int Height(Node node)
+        {
+            if (node == null)
+            {
+                return 0;
+            }
+
+            return node.Height;
         }
 
         public void EachInOrder(Action<T> action)
         {
-            throw new InvalidOperationException();
+            this.EachInOrder(this.Root, action);
+        }
+
+        private void EachInOrder(Node node, Action<T> action)
+        {
+            if (node == null)
+            {
+                return;
+            }
+
+            this.EachInOrder(node.Left, action);
+            action(node.Value);
+            this.EachInOrder(node.Right, action);
         }
     }
 }
