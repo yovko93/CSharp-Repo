b7748332ab18fe1195dee43754e485aaf747e879
Yovko Garkov <iovko1993@gmail.com>
2023-12-07 15:45:30 +0200

    Add Data Structures Advanced Data Structures Augmentation skeleton

---
+++ Data_Structures_Advanced_(with_C#)/07.Data-Structures-Advanced-Data-Structures-Augmentation-Lab/PeopleCollection/PeopleCollectionSlow.cs
@@ -0,0 +1,44 @@
+﻿namespace CollectionOfPeople
+{
+    using System.Collections.Generic;
+
+    public class PeopleCollectionSlow : IPeopleCollection
+    {
+        public int Count => throw new System.NotImplementedException();
+
+        public bool Add(string email, string name, int age, string town)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool Delete(string email)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Person Find(string email)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<Person> FindPeople(string emailDomain)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<Person> FindPeople(string name, string town)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<Person> FindPeople(int startAge, int endAge)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<Person> FindPeople(int startAge, int endAge, string town)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Data_Structures_Advanced_(with_C#)/07.Data-Structures-Advanced-Data-Structures-Augmentation-Lab/PeopleCollection/Person.cs
@@ -0,0 +1,12 @@
+﻿namespace CollectionOfPeople
+{
+    using System;
+
+    public class Person
+    {
+        public string Email { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Town { get; set; }
+    }
+}
